041a5869c5b00526350a0fd42597d4a705b359a7
puruscor <puruscor@users.noreply.github.com>
2019-03-18 15:47:20 +0200

    Add pose to res-info tooltips

--- src/haven/MapView.java
+++ src/haven/MapView.java
@@ -26,28 +26,6 @@
 
 package haven;
 
-import static haven.MCache.tilesz;
-import static haven.OCache.posres;
-
-import java.awt.Color;
-import java.awt.event.KeyEvent;
-import java.lang.ref.Reference;
-import java.lang.ref.WeakReference;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.WeakHashMap;
-
-import javax.media.opengl.GL;
-
 import haven.GLProgram.VarID;
 import haven.automation.AreaSelectCallback;
 import haven.automation.GobSelectCallback;
@@ -55,8 +33,20 @@ import haven.automation.MusselPicker;
 import haven.automation.SteelRefueler;
 import haven.pathfinder.PFListener;
 import haven.pathfinder.Pathfinder;
-import haven.purus.pbot.PBotAPI;
+import haven.purus.pbot.PBotUtils;
 import haven.resutil.BPRadSprite;
+
+import javax.media.opengl.GL;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+
+import static haven.MCache.tilesz;
+import static haven.OCache.posres;
 
 public class MapView extends PView implements DTarget, Console.Directory, PFListener {
     public static boolean clickdb = false;
@@ -2033,9 +2023,21 @@ public class MapView extends PView implements DTarget, Console.Directory, PFList
                                     for(Gob.Overlay o:inf.gob.ols) {
                                         try {
                                             if(o.res != null && o.res.get() != null)
-                                                tooltip += String.format("\n" + o.res.get().name);
+                                                tooltip += String.format("\noverlay: " + o.res.get().name);
                                         } catch(Loading l) {
 
+                                        }
+                                    }
+                                    Drawable d = inf.gob.getattr(Drawable.class);
+
+                                    if(d instanceof Composite) {
+                                        Composite comp = (Composite)d;
+                                        for(ResData rd:comp.prevposes) {
+                                            try {
+                                                tooltip += String.format("\npose: " + rd.res.get().name);
+                                            } catch(Loading l) {
+
+                                            }
                                         }
                                     }
                                     return;
