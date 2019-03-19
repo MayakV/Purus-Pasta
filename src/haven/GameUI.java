1ae4f55b88d79ea8868c1ef0de9bd7cfe2734fd5
Дмитрий Шаров <dsharov96@gmail.com>
2019-03-19 11:51:29 +0300

    Revert "Added Autokin"
    This reverts commit 06939eda66e4d3479c8f850dbaed7b8fb10ca1f7.

--- src/haven/GameUI.java
+++ src/haven/GameUI.java
@@ -1263,7 +1263,6 @@ public class GameUI extends ConsoleHost implements Console.Directory {
             msg("Autodrink " + (Config.autodrink?"Enabled!":"Disabled!"), Color.white);
             return true;
         } else if (ev.isControlDown() && ev.getKeyCode() == KeyEvent.VK_K){
-            // AutoKin
             Thread t = new Thread(new AutoKin(this), "AutoKin");
             t.start();
             return true;
